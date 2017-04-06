34291244499c5689e535a5b97e94f8cd2f360d33
Mike Bass <Mike Bass>
2017-04-06 18:28:32 -0400

    final

---
+++ liri.js
@@ -0,0 +1,132 @@
+var keys = require("./keys.js");
+var fs = require('fs');
+var twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+
+
+//Creates a function for finding artist name from spotify
+var artistNames = function(artist) {
+  return artist.name;
+};
+
+//Function for finding songs on Spotify
+var spotifyThis = function(songName) {
+  //If it doesn't find a song, find Peter Gabriel's Sledgehammer
+  if (songName === undefined) {
+    songName = 'Sledgehammer';
+  };
+
+  spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if (err) {
+      console.log('Error occurred: ' + err);
+      return;
+    }
+
+    var songs = data.tracks.items;
+    var data = []; //empty array to hold data
+
+    for (var i = 0; i < songs.length; i++) {
+      data.push({
+        'artist(s)': songs[i].artists.map(artistNames),
+        'song name: ': songs[i].name,
+        'preview song: ': songs[i].preview_url,
+        'album: ': songs[i].album.name,
+      });
+    }
+    console.log(data);
+  });
+};
+
+
+var tweets = function() {
+  var client = new twitter(keys.twitterKeys);
+
+  var params = { screen_name: 'SlyAteTweety', count: 20 };
+
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+
+    if (!error) {
+      var data = []; //empty array to hold data
+      for (var i = 0; i < tweets.length; i++) {
+        data.push({
+            'created at: ' : tweets[i].created_at,
+            'Tweets: ' : tweets[i].text,
+        });
+      }
+      console.log(data);
+    }
+  });
+};
+
+var movieThis = function(movieName) {
+
+  if (movieName === undefined) {
+    movieName = 'Mr Nobody';
+  }
+
+  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&r=json";
+
+  request(urlHit, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = [];
+      var jsonData = JSON.parse(body);
+
+      data.push({
+      'Title: ' : jsonData.Title,
+      'Year: ' : jsonData.Year,
+      'Rated: ' : jsonData.Rated,
+      'IMDB Rating: ' : jsonData.imdbRating,
+      'Country: ' : jsonData.Country,
+      'Language: ' : jsonData.Language,
+      'Plot: ' : jsonData.Plot,
+      'Actors: ' : jsonData.Actors,
+      'Rotten Tomatoes Rating: ' : jsonData.tomatoRating,
+  });
+      console.log(data);
+}
+  });
+
+}
+
+var doWhatItSays = function() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    console.log(data);
+    var dataArr = data.split(',')
+
+    if (dataArr.length == 2) {
+      pick(dataArr[0], dataArr[1]);
+    } else if (dataArr.length == 1) {
+      pick(dataArr[0]);
+    }
+
+  });
+}
+
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+    case 'my-tweets':
+      tweets();
+      break;
+    case 'spotify-this-song':
+      spotifyThis(functionData);
+      break;
+    case 'movie-this':
+      movieThis(functionData);
+      break;
+    case 'do-what-it-says':
+      doWhatItSays();
+      break;
+    default:
+      console.log('LIRI doesn\'t know that');
+  }
+}
+
+//run this on load of js file
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
+
+
